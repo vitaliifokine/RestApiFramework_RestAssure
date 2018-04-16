84317c6990b87500426cb908a5957ee80840a3e0
vitaliifokine <Vf281992>
2018-04-16 11:38:41 -0400

    Initial commit

---
+++ src/RestApiTests.java
@@ -0,0 +1,50 @@
+import com.jayway.restassured.RestAssured;
+import com.jayway.restassured.path.json.JsonPath;
+import com.jayway.restassured.response.Response;
+import com.jayway.restassured.specification.RequestSpecification;
+import io.qameta.allure.Feature;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class RestApiTests {
+    
+    @Feature("REST API - LAUREATS")
+    @Test
+    public void getNobelLaureat() {
+        RestAssured.baseURI = "http://api.nobelprize.org";
+        RequestSpecification httpRequest = RestAssured.given();
+        Response response = httpRequest.get("/v1/laureate.json");
+        String responseBody = response.getBody().asString();
+        assertThat(response.getBody().asString(),
+                containsString("Paul Hermann"));
+    }
+
+    @Feature("REST API - CITIES FROM COUNTRIES")
+    @Test
+    public void getCityFromCountryStatusValidation() {
+        RestAssured.baseURI = "http://api.nobelprize.org";
+        RequestSpecification httpRequest = RestAssured.given();
+        Response response = httpRequest.get("/v1/laureate.json");
+        int statusCode = response.getStatusCode();
+        Assert.assertEquals(statusCode /*actual value*/, 200 /*expected value*/, "Correct status code returned");
+    }
+
+    @Feature("REST API - NOBEL PRIZE LAUREAT")
+    @Test
+    public void getFirstLaureatLastNameJSON() {
+        RestAssured.baseURI = "http://api.nobelprize.org";
+        RequestSpecification httpRequest = RestAssured.given();
+        Response response = httpRequest.get("/v1/laureate.json");
+        JsonPath jsonPathEvaluator = response.jsonPath();
+        List<String> firstNamesOfAllLaureates = jsonPathEvaluator.getList("laureates.firstname");
+        List<String> lastNamesOfAllLaureates = jsonPathEvaluator.getList("laureates.surname");
+        Assert.assertEquals(firstNamesOfAllLaureates.get(0) + " " +
+                        lastNamesOfAllLaureates.get(0), "Wilhelm Conrad Röntgen",
+                "Correct laureate first & last name received in the Response");
+    }
+}
