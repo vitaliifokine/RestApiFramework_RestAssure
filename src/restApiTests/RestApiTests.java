1b5364b968a8b3a2685c1c55c0a621d3dbb42ad8
vitaliifokine <vf281992>
2018-11-11 11:54:10 -0500

    Updates

--- src/restApiTests/RestApiTests.java
+++ src/restApiTests/RestApiTests.java
@@ -1,3 +1,5 @@
+package restApiTests;
+
 import com.jayway.restassured.RestAssured;
 import com.jayway.restassured.path.json.JsonPath;
 import com.jayway.restassured.response.Response;
@@ -46,5 +48,48 @@ public class RestApiTests {
         Assert.assertEquals(firstNamesOfAllLaureates.get(0) + " " +
                         lastNamesOfAllLaureates.get(0), "Wilhelm Conrad Röntgen",
                 "Correct laureate first & last name received in the Response");
+    }
+
+    @Feature("REST API - NOBEL PRIZE LAUREAT")
+    @Test
+    public void getFirstLaureatLastNameJSON1() {
+        RestAssured.baseURI = "http://api.nobelprize.org";
+        RequestSpecification httpRequest = RestAssured.given();
+        Response response = httpRequest.get("/v1/laureate.json");
+        JsonPath jsonPathEvaluator = response.jsonPath();
+        List<String> firstNamesOfAllLaureates = jsonPathEvaluator.getList("laureates.firstname");
+        List<String> lastNamesOfAllLaureates = jsonPathEvaluator.getList("laureates.surname");
+       for (int ind=1; ind<firstNamesOfAllLaureates.size(); ind++){
+           System.out.println(firstNamesOfAllLaureates.get(ind) + " " + lastNamesOfAllLaureates.get(ind));
+       }
+
+    }
+
+    @Feature("REST API - NOBEL PRIZE LAUREAT")
+    @Test
+    public void getFirstLaureatLastNameJSON2() {
+        RestAssured.baseURI = "http://api.nobelprize.org";
+        RequestSpecification httpRequest = RestAssured.given();
+        Response response = httpRequest.get("/v1/laureate.json");
+        response.print();
+
+
+    }
+
+    @Feature("REST API - NOBEL PRIZE LAUREAT")
+    @Test
+    public void getFirstLaureatLastNameJSON3() {
+        RestAssured.baseURI = "http://api.nobelprize.org";
+        RequestSpecification httpRequest = RestAssured.given();
+        Response response = httpRequest.get("/v1/laureate.json");
+        JsonPath jsonPathEvaluator = response.jsonPath();
+        List<String> firstNamesOfAllLaureates = jsonPathEvaluator.getList("laureates.firstname");
+        List<String> lastNamesOfAllLaureates = jsonPathEvaluator.getList("laureates.surname");
+        List<String> died = jsonPathEvaluator.getList("laureates.diedCountry");
+        for (int ind=1; ind<firstNamesOfAllLaureates.size(); ind++){
+            if (died.get(ind)=="Germany") {
+                System.out.println(firstNamesOfAllLaureates.get(ind) + " " + lastNamesOfAllLaureates.get(ind) + " " + died.get(ind));
+            }
+        }
     }
 }
